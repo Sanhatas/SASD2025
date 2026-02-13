24c219997993c9ab69f72523ff0af7eb4ace3ea8
Sanhatas <sanhatas.jung@g.swu.ac.th>
2026-02-13 16:37:41 +0700

    Added: SkiaLiteUI project include all file in 05 Folder in Google Drive

---
+++ 05/RectWidget.cs
@@ -0,0 +1,49 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+
+namespace SkiaLiteUI;
+
+public abstract class Widget
+{
+    public virtual void Act(float deltaTime) { }
+    public abstract void Draw(SKCanvas canvas);
+}
+
+public class RectWidget
+{
+    public Vector Position { get; }
+    public Vector Size { get; }
+    public SKColor Color { get; init; } = SKColors.White;
+    public float Radius { get; set; } = 0;
+
+    public RectWidget(Vector origin, Vector size)
+    {
+        Position = origin;
+        Size = size;
+    }
+
+    //public void Act(float deltaTime)
+    //{
+    //}
+
+    //public void Draw(SKCanvas canvas)
+    //{
+    //}
+
+    public static explicit operator SKRect(RectWidget r)
+    {
+        return new SKRect(  r.Position.X, r.Position.Y, // todo: Symmetry Format
+                            r.Position.X + r.Size.X,
+                            r.Position.Y + r.Size.Y);
+    }
+
+    // todo: Where this method should move into ?
+    public static RectWidget CreateRandom(Random rand, Vector max, Vector size)
+    {
+        var widget = new RectWidget(rand.NextVector(max), size)
+        { Color = new SKColor((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256)) };
+        // todo: Extract & Move Method: rand.NextColor()
+        return widget;
+    }
+}
